c76270b6fa9180db80145917791c3ce06f3c9aba
artyshokas <artiom.harchenko@gmail.com>
2022-11-24 09:09:43 +0200

    local settings

--- ptu5_postit/ptu5_postit/urls.py
+++ ptu5_postit/ptu5_postit/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('postit_api.urls'))
+    # path('', include('postit_api.urls')),
 ]
