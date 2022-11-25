291e4bb5d68800ae7f5e3a6c9b885b24d9034828
artyshokas <artiom.harchenko@gmail.com>
2022-11-25 13:28:44 +0200

    post comments. user limited and post limited comment view and cru with no D

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers
 from . import models
 
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    post = serializers.ReadOnlyField(source='post_id')
+
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'post', 'body', 'user', 'user_id', 'created_at') # 'post' yra foregn key  
+
+
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
+    comments = CommentSerializer(many=True)
+
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at')
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at', 'comments')  
